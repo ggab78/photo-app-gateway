60d5ffa83f55e1b9c5dff275fce51399fdb9e623
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2021-04-29 22:49:23 +0200

    add spring boot actuator to monitor gateway

---
+++ src/main/java/com/gabriel/photoappgateway/WebSecurity.java
@@ -0,0 +1,20 @@
+package com.gabriel.photoappgateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class WebSecurity {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity http) {
+
+        return http.authorizeExchange().pathMatchers("/actuator/**").permitAll()
+                .and().build();
+
+    }
+
+}
